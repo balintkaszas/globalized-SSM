f2878b5f02e9a5c1b48b62221b367fe9c9cf353c
balintkaszas <bkaszas@ethz.ch>
2025-02-13 17:32:38 +0100

    Gitignore template

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='taylor_to_pade',
+    version='0.1.0',
+    author='Balint Kaszas',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',  
+        'scipy',  
+        'matplotlib',
+        'sympy'
+    ],
+    python_requires='>=3.7',
+    include_package_data=True
+)
